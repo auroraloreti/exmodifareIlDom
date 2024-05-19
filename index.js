72b18efe35e8601ce92e0f32bd83fdb1aa79e941
auroraloreti <loretiaurora16@gmail.com>
2024-05-19 20:02:10 +0200

    svolgimento

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-const addProduct = () => {
+
+
+const addProduct = () => { 
+  const ul = document.querySelector("#my-ul");
+  const li = document.createElement("li")
+ 
+
+  const checkbox = document.createElement("INPUT");
+  checkbox.setAttribute("type", "checkbox");
+  
+  ul.appendChild( checkbox)
+  ul.appendChild( li )
+  
+  ul.style.display = "flex"
+  li.style.listStyleType = "none";
+
+  const input = document.querySelector("#my-value")
+  const value = input.value;
+  if(value != ""){
+    console.log(checkbox)
+    li.innerText = value
+    
+  }
+  
   // ...
 };
